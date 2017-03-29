222d26fc640f36a06cd4c3328d8b310ac6d3592a
Joe W <wjoe113@vt.edu>
2017-03-28 22:17:22 -0400

    committing parts 2 & 3, need a 1 day extension plz
    ^

--- Homework2/SnakesAndLadders/TransitionMatrix.h
+++ Homework2/SnakesAndLadders/TransitionMatrix.h
@@ -1,15 +1,18 @@
-int size = 10;  //TODO
+int size = 101;  //TODO - 101
 Eigen::MatrixXf TransitionMatrix(size, size);
 Eigen::VectorXf v(size);
 
-unsigned int ROLLS = 10; //TODO
+unsigned int ROLLS = 10; //TODO - "They see me rolling!"
 
-double prob = (1.0/6.0);  //TODO
+double prob = (1.0/6.0);  //TODO - 1/6
 
 void SetTransitionMatrix()
 {
 	TransitionMatrix.setZero();
 	//TransitionMatrix(0, 1) = prob;
+	// rows = 
+	// columns = 
+
 	for (int i = 0; i < TransitionMatrix.rows(); i++)
 	{
 		if (i < TransitionMatrix.rows() - 6)
@@ -49,13 +52,255 @@ void SetTransitionMatrix()
 		}
 		else if (i == TransitionMatrix.rows() - 2)
 		{
-			TransitionMatrix(i, i + 1) = prob * 6;
+			TransitionMatrix(i, i + 1) = 1; // You're about to win, assuming the world doesn't end before your next turn...
 		}
 		else if (i == TransitionMatrix.rows() - 1)
 		{
-			//YOU WIN!!!
+			TransitionMatrix(i, i) = 1; //YOU WIN!!! And the world didn't end!
 		}
 	}
+
+	///Part c - comment out for null matrix
+	///the two sections can be combined, but for readability I left them seperated
+	if (size > 99)
+	{
+		//Ladder from 3 to 19
+		TransitionMatrix(0, 19) = prob;
+		TransitionMatrix(1, 19) = prob;
+		TransitionMatrix(2, 19) = prob;
+
+		//Ladder from 15 to 37
+		TransitionMatrix(9, 37) = prob;
+		TransitionMatrix(10, 37) = prob;
+		TransitionMatrix(11, 37) = prob;
+		TransitionMatrix(12, 37) = prob;
+		TransitionMatrix(13, 37) = prob;
+		TransitionMatrix(14, 37) = prob;
+
+		//Ladder from 22 to 42
+		TransitionMatrix(16, 42) = prob;
+		TransitionMatrix(17, 42) = prob;
+		TransitionMatrix(18, 42) = prob;
+		TransitionMatrix(19, 42) = prob;
+		TransitionMatrix(20, 42) = prob;
+		TransitionMatrix(21, 42) = prob;
+
+		//Ladder from 25 to 64
+		TransitionMatrix(19, 64) = prob;
+		TransitionMatrix(20, 64) = prob;
+		TransitionMatrix(21, 64) = prob;
+		TransitionMatrix(22, 64) = prob;
+		TransitionMatrix(23, 64) = prob;
+		TransitionMatrix(24, 64) = prob;
+
+		//Ladder from 53 to 74
+		TransitionMatrix(36, 74) = prob;
+		TransitionMatrix(37, 74) = prob;
+		TransitionMatrix(38, 74) = prob;
+		TransitionMatrix(39, 74) = prob;
+		TransitionMatrix(40, 74) = prob;
+		TransitionMatrix(41, 74) = prob;
+
+		//Ladder from 53 to 74
+		TransitionMatrix(47, 74) = prob;
+		TransitionMatrix(48, 74) = prob;
+		TransitionMatrix(49, 74) = prob;
+		TransitionMatrix(50, 74) = prob;
+		TransitionMatrix(51, 74) = prob;
+		TransitionMatrix(52, 74) = prob;
+
+		//Ladder from 63 to 86
+		TransitionMatrix(58, 86) = prob;
+		TransitionMatrix(59, 86) = prob;
+		TransitionMatrix(60, 86) = prob;
+		TransitionMatrix(61, 86) = prob;
+		TransitionMatrix(62, 86) = prob;
+		TransitionMatrix(63, 86) = prob;
+
+		//Ladder from 76 to 91
+		TransitionMatrix(70, 91) = prob;
+		TransitionMatrix(71, 91) = prob;
+		TransitionMatrix(72, 91) = prob;
+		TransitionMatrix(73, 91) = prob;
+		TransitionMatrix(74, 91) = prob;
+		TransitionMatrix(75, 91) = prob;
+
+		//Ladder from 84 to 98
+		TransitionMatrix(78, 98) = prob;
+		TransitionMatrix(79, 98) = prob;
+		TransitionMatrix(80, 98) = prob;
+		TransitionMatrix(81, 98) = prob;
+		TransitionMatrix(82, 98) = prob;
+		TransitionMatrix(83, 98) = prob;
+
+		//Snake from 11 to 7
+		TransitionMatrix(5, 7) = prob*2;
+		TransitionMatrix(6, 7) = prob*2;
+		TransitionMatrix(7, 7) = prob;
+		TransitionMatrix(8, 7) = prob;
+		TransitionMatrix(9, 7) = prob;
+		TransitionMatrix(10, 7) = prob;
+
+		//Snake from 18 to 13
+		TransitionMatrix(12, 13) = prob*2;
+		TransitionMatrix(13, 13) = prob;
+		TransitionMatrix(14, 13) = prob;
+		TransitionMatrix(15, 13) = prob;
+		TransitionMatrix(16, 13) = prob;
+		TransitionMatrix(17, 13) = prob;
+
+		//Snake from 28 to 12
+		TransitionMatrix(22, 12) = prob;
+		TransitionMatrix(23, 12) = prob;
+		TransitionMatrix(24, 12) = prob;
+		TransitionMatrix(25, 12) = prob;
+		TransitionMatrix(26, 12) = prob;
+		TransitionMatrix(27, 12) = prob;
+
+		//Snake from 36 to 34
+		TransitionMatrix(30, 34) = prob*2;
+		TransitionMatrix(31, 34) = prob*2;
+		TransitionMatrix(32, 34) = prob*2;
+		TransitionMatrix(33, 34) = prob*2;
+		TransitionMatrix(34, 34) = prob;
+		TransitionMatrix(35, 34) = prob;
+
+		//Snake from 77 to 16 - Dang that sucks!
+		TransitionMatrix(71, 16) = prob;
+		TransitionMatrix(72, 16) = prob;
+		TransitionMatrix(73, 16) = prob;
+		TransitionMatrix(74, 16) = prob;
+		TransitionMatrix(75, 16) = prob;
+		TransitionMatrix(76, 16) = prob;
+
+		//Snake from 47 to 26
+		TransitionMatrix(41, 26) = prob;
+		TransitionMatrix(42, 26) = prob;
+		TransitionMatrix(43, 26) = prob;
+		TransitionMatrix(44, 26) = prob;
+		TransitionMatrix(45, 26) = prob;
+		TransitionMatrix(46, 26) = prob;
+
+		//Snake from 83 to 39
+		TransitionMatrix(77, 39) = prob;
+		TransitionMatrix(78, 39) = prob;
+		TransitionMatrix(79, 39) = prob;
+		TransitionMatrix(80, 39) = prob;
+		TransitionMatrix(81, 39) = prob;
+		TransitionMatrix(82, 39) = prob;
+
+		//Snake from 92 to 75
+		TransitionMatrix(86, 75) = prob;
+		TransitionMatrix(87, 75) = prob;
+		TransitionMatrix(88, 75) = prob;
+		TransitionMatrix(89, 75) = prob;
+		TransitionMatrix(90, 75) = prob;
+		TransitionMatrix(91, 75) = prob;
+
+		//Snake from 99 to 70
+		TransitionMatrix(93, 70) = prob;
+		TransitionMatrix(94, 70) = prob;
+		TransitionMatrix(95, 70) = prob;
+		TransitionMatrix(96, 70) = prob;
+		TransitionMatrix(97, 70) = prob;
+		TransitionMatrix(98, 70) = prob;
+
+		for (int i = 0; i < TransitionMatrix.rows(); i++) //There is a zero probability you'll stand on a snake or ladder
+		{
+			//Ladders
+			TransitionMatrix(i, 3) = 0.0;
+			TransitionMatrix(i, 15) = 0.0;
+			TransitionMatrix(i, 22) = 0.0;
+			TransitionMatrix(i, 25) = 0.0;
+			TransitionMatrix(i, 41) = 0.0;
+			TransitionMatrix(i, 53) = 0.0;
+			TransitionMatrix(i, 63) = 0.0;
+			TransitionMatrix(i, 76) = 0.0;
+			TransitionMatrix(i, 84) = 0.0;
+			//Snakes
+			TransitionMatrix(i, 11) = 0.0;
+			TransitionMatrix(i, 18) = 0.0;
+			TransitionMatrix(i, 28) = 0.0;
+			TransitionMatrix(i, 36) = 0.0;
+			TransitionMatrix(i, 77) = 0.0;
+			TransitionMatrix(i, 47) = 0.0;
+			TransitionMatrix(i, 83) = 0.0;
+			TransitionMatrix(i, 92) = 0.0;
+			TransitionMatrix(i, 99) = 0.0;
+		}
+	}
+	
+	//part c - old
+	/*for (int i = 0; i < TransitionMatrix.rows() - 6; i++)
+	{
+		if (i == 3)
+		{
+			TransitionMatrix(i, 19) = 1;
+		}
+		else if (i + 1 == 3)
+		{
+			TransitionMatrix(i, 19) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+		else if (i + 2 == 3)
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, 19) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+		else if (i + 3 == 3)
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, 19) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+		else if (i + 4 == 3)
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, 19) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+		else if (i + 5 == 3)
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, 19) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+		else if (i + 6 == 3)
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, 19) = prob;
+		}
+		else
+		{
+			TransitionMatrix(i, i + 1) = prob;
+			TransitionMatrix(i, i + 2) = prob;
+			TransitionMatrix(i, i + 3) = prob;
+			TransitionMatrix(i, i + 4) = prob;
+			TransitionMatrix(i, i + 5) = prob;
+			TransitionMatrix(i, i + 6) = prob;
+		}
+	}*/
 
 	//TODO
 	std::cout << TransitionMatrix << std::endl;
--- Homework2/queues/main.cpp
+++ Homework2/queues/main.cpp
@@ -32,55 +32,101 @@ int main(int argc, char* argv[])
 
    for (int cur_lambda = 3; cur_lambda < 34; cur_lambda++)
    {
-      
 	   //TODO Create MM1_Queue objects to capture the airport senario.
+
+	   MM1_Queue idCheck;
+	   idCheck.set_file_names("idCheck_log.txt", "idCheck_wait.txt", "idCheck_service.txt");
+	   idCheck.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   idCheck.set_mu(53);
+	   idCheck.initialize();
+	   idCheck.set_seed(rd(), rd());	// I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
+
+	   MM1_Queue security1;
+	   security1.set_file_names("Security1_log.txt", "Security1_wait.txt", "Security1_service.txt");
+	   security1.set_lambda(6);
+	   security1.set_mu(20);
+	   security1.autogenerate_new_arrivals(false);
+	   security1.initialize();
+	   security1.set_seed(rd(), rd());
+
+	   MM1_Queue security2;
+	   security2.set_file_names("Security2_log.txt", "Security2_wait.txt", "Security2_service.txt");
+	   security2.set_lambda(6);
+	   security2.set_mu(20);
+	   security2.autogenerate_new_arrivals(false);
+	   security2.initialize();
+	   security2.set_seed(rd(), rd());
+
+	   MM1_Queue security3;
+	   security3.set_file_names("Security3_log.txt", "Security3_wait.txt", "Security3_service.txt");
+	   security3.set_lambda(6);
+	   security3.set_mu(20);
+	   security3.autogenerate_new_arrivals(false);
+	   security3.initialize();
+	   security3.set_seed(rd(), rd());
+
+	   MM1_Queue boardPlane;
+	   boardPlane.set_file_names("boardPlane_log.txt", "boardPlane_wait.txt", "boardPlane_service.txt");
+	   boardPlane.set_lambda(6);
+	   boardPlane.set_mu(80);
+	   boardPlane.initialize();
+	   boardPlane.set_seed(rd(), rd());
 
 	   //************************************************************
 
+	   for (; 
+			//TODO: add is_within_error_range check
+			!idCheck.is_within_error_range(0.002) ||
+			!security1.is_within_error_range(0.002) ||
+			!security2.is_within_error_range(0.002) ||
+			!security3.is_within_error_range(0.002) ||
+			!boardPlane.is_within_error_range(0.002)
+		   ;)
+	   {
+		   Customer cust = idCheck.process_next_event();		// =  TODO: process next event;
+		   Customer cust2 = security1.process_next_event();		// =  TODO: process next event;
+		   Customer cust3 = security2.process_next_event();		// =  TODO: process next event;
+		   Customer cust4 = security3.process_next_event();		// =  TODO: process next event;
+		   //TODO: one more process_next_event for the last object.
+		   Customer cust5 = boardPlane.process_next_event();
 
-   for (; 
-		//TODO: add is_within_error_range check
-       ;)
-   {
-	   Customer cust  ;    // =  TODO: process next event;
-	   Customer cust2  ;   // =  TODO: process next event;
-	   Customer cust3  ;   // =  TODO: process next event;
-	   Customer cust4  ;   // =  TODO: process next event;
-	   //TODO: one more process_next_event for the last object.
+		   if (cust.get_type() == Customer::COMPLETED())
+		   {
+			  switch(next)
+			  {
+				case 0:
+					//TODO add_external_arrival() for your security gates;
+					security1.add_external_arrival();
+					break;
+				case 1:
+					//TODO add_external_arrival() for your security gates;
+					security2.add_external_arrival();
+					break;
+				case 2:
+					//TODO add_external_arrival() for your security gates;
+					security3.add_external_arrival();
+					break;
+			  }
+			  next++;
+			  if (next%3==0) next = 0;
+		   }
+		   if (cust2.get_type() == Customer::COMPLETED() || cust3.get_type() == Customer::COMPLETED() || cust4.get_type() == Customer::COMPLETED())
+		   {
+			   //TODO add_external_arrival(); on your final boarding MM1_Queue object
+			   boardPlane.add_external_arrival();
+		   }
+	   }
 
-       if (cust.get_type() == Customer::COMPLETED())
-       {
-          switch(next)
-          {
-            case 0:
-				//TODO add_external_arrival() for your security gates;
-                 break;
-            case 1:
-				//TODO add_external_arrival() for your security gates;
-                 break;
-            case 2:
-                //TODO add_external_arrival() for your security gates;
-                 break;
-          }
-          next++;
-          if (next%3==0) next = 0;
-       }
-       if (cust2.get_type() == Customer::COMPLETED() || cust3.get_type() == Customer::COMPLETED() || cust4.get_type() == Customer::COMPLETED())
-       {
-		   //TODO add_external_arrival(); on your final boarding MM1_Queue object
-       }
-   }
+		//TODO Output statistics airport senario.
+   
+		idCheck.output(); cout << "*********" << endl;
+		security1.output(); cout << "*********" << endl;
+		security2.output(); cout << "*********" << endl;
+		security3.output(); cout << "*********" << endl;
+		boardPlane.output(); cout << "*********" << endl;
 
-
-
-   //TODO Output statistics airport senario.
-
-
-
-   //**************************************************************************
-
-
-
+		//**************************************************************************
+   
    }
 
    return(0);
