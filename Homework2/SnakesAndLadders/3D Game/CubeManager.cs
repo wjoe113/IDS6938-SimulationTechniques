c8bc0b04e7ec20f3df88b0d13c40ae1d5399d071
Joe W <wjoe113@vt.edu>
2017-03-30 23:12:21 -0400

    Extra features photos
    Hopefully final submit

--- Homework2/SnakesAndLadders/3D Game/CubeManager.cs
+++ Homework2/SnakesAndLadders/3D Game/CubeManager.cs
@@ -26,10 +26,15 @@ public class CubeManager : MonoBehaviour {
     public Material blueMat;
     [Tooltip("Green Material")]
     public Material greenMat;
+    [Tooltip("Player object")]
+    public GameObject player;
     private Material defaultMaterial;
     public string cubeNum;
     public bool meYellow;
     public bool meBlue;
+    public bool iAmYellow;
+    public int myOrderNum;
+    private int orderRec;
 
     void Start ()
     {
@@ -37,6 +42,9 @@ public class CubeManager : MonoBehaviour {
         string myName = gameObject.name;
         meYellow = false;
         meBlue = false;
+        iAmYellow = false;
+        myOrderNum = 0;
+        orderRec = 0;
 
         for (int i = 6; i < myName.Length; i++)
         {
@@ -53,128 +61,195 @@ public class CubeManager : MonoBehaviour {
 
     private void Update()
     {
-        string mousedCubeName = Camera.main.GetComponent<MouseOverCube>().savedName;
+        //string mousedCubeNum = Camera.main.GetComponent<MouseOverCube>().savedName;
+        string playerCubeNum = player.GetComponent<PlayerManager>().playerCurrentTile;
 
-        if (mousedCubeName == "95" && cubeNum == "100" && meYellow)
+        if (playerCubeNum == "95" && cubeNum == "100" && meYellow)
         {
             gameObject.GetComponent<Renderer>().material = yellowMat2;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (mousedCubeName == "96" && cubeNum == "100" && meYellow)
+        else if (playerCubeNum == "96" && cubeNum == "100" && meYellow)
         {
             gameObject.GetComponent<Renderer>().material = yellowMat3;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (mousedCubeName == "97" && cubeNum == "100" && meYellow)
+        else if (playerCubeNum == "97" && cubeNum == "100" && meYellow)
         {
             gameObject.GetComponent<Renderer>().material = yellowMat4;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (mousedCubeName == "98" && cubeNum == "100" && meYellow)
+        else if (playerCubeNum == "98" && cubeNum == "100" && meYellow)
         {
             gameObject.GetComponent<Renderer>().material = yellowMat5;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
         else if (meYellow && cubeNum != "03" && cubeNum != "15" && cubeNum != "22" && cubeNum != "25" && cubeNum != "41" && cubeNum != "53" && cubeNum != "63" && cubeNum != "76" && cubeNum != "84"
             && cubeNum != "11" && cubeNum != "18" && cubeNum != "28" && cubeNum != "36" && cubeNum != "77" && cubeNum != "47" && cubeNum != "83" && cubeNum != "92" && cubeNum != "99"
             && cubeNum != "07" && cubeNum != "13" && cubeNum != "34"/* && cubeNum != "16" && cubeNum != "26" && cubeNum != "39" && cubeNum != "75" && cubeNum != "70"*/
-            && mousedCubeName != "03" && mousedCubeName != "15" && mousedCubeName != "22" && mousedCubeName != "25" && mousedCubeName != "41" && mousedCubeName != "53" && mousedCubeName != "63" && mousedCubeName != "76" && mousedCubeName != "84"
-            && mousedCubeName != "11" && mousedCubeName != "18" && mousedCubeName != "28" && mousedCubeName != "36" && mousedCubeName != "77" && mousedCubeName != "47" && mousedCubeName != "83" && mousedCubeName != "92" && mousedCubeName != "99")
+            && playerCubeNum != "03" && playerCubeNum != "15" && playerCubeNum != "22" && playerCubeNum != "25" && playerCubeNum != "41" && playerCubeNum != "53" && playerCubeNum != "63" && playerCubeNum != "76" && playerCubeNum != "84"
+            && playerCubeNum != "11" && playerCubeNum != "18" && playerCubeNum != "28" && playerCubeNum != "36" && playerCubeNum != "77" && playerCubeNum != "47" && playerCubeNum != "83" && playerCubeNum != "92" && playerCubeNum != "99")
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "19" && (mousedCubeName == "01" || mousedCubeName == "02")) //Ladders
+        else if (meYellow && cubeNum == "19" && (playerCubeNum == "01" || playerCubeNum == "02")) //Ladders
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "37" && (mousedCubeName == "09" || mousedCubeName == "10" || mousedCubeName == "12" || mousedCubeName == "13" || mousedCubeName == "14"))
+        else if (meYellow && cubeNum == "37" && (playerCubeNum == "09" || playerCubeNum == "10" || playerCubeNum == "12" || playerCubeNum == "13" || playerCubeNum == "14"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "42" && (mousedCubeName == "16" || mousedCubeName == "17" || mousedCubeName == "19" || mousedCubeName == "20" || mousedCubeName == "21"))
+        else if (meYellow && cubeNum == "42" && (playerCubeNum == "16" || playerCubeNum == "17" || playerCubeNum == "19" || playerCubeNum == "20" || playerCubeNum == "21"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "64" && (mousedCubeName == "19" || mousedCubeName == "20" || mousedCubeName == "21" || mousedCubeName == "23" || mousedCubeName == "24"))
+        else if (meYellow && cubeNum == "64" && (playerCubeNum == "19" || playerCubeNum == "20" || playerCubeNum == "21" || playerCubeNum == "23" || playerCubeNum == "24"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "73" && (mousedCubeName == "35" || mousedCubeName == "37" || mousedCubeName == "38" || mousedCubeName == "39" || mousedCubeName == "40"))
+        else if (meYellow && cubeNum == "73" && (playerCubeNum == "35" || playerCubeNum == "37" || playerCubeNum == "38" || playerCubeNum == "39" || playerCubeNum == "40"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "74" && (mousedCubeName == "48" || mousedCubeName == "49" || mousedCubeName == "50" || mousedCubeName == "51" || mousedCubeName == "52"))
+        else if (meYellow && cubeNum == "74" && (playerCubeNum == "48" || playerCubeNum == "49" || playerCubeNum == "50" || playerCubeNum == "51" || playerCubeNum == "52"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "86" && (mousedCubeName == "57" || mousedCubeName == "58" || mousedCubeName == "59" || mousedCubeName == "60" || mousedCubeName == "61" || mousedCubeName == "62"))
+        else if (meYellow && cubeNum == "86" && (playerCubeNum == "57" || playerCubeNum == "58" || playerCubeNum == "59" || playerCubeNum == "60" || playerCubeNum == "61" || playerCubeNum == "62"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "91" && (mousedCubeName == "70" || mousedCubeName == "71" || mousedCubeName == "72" || mousedCubeName == "73" || mousedCubeName == "74" || mousedCubeName == "75"))
+        else if (meYellow && cubeNum == "91" && (playerCubeNum == "70" || playerCubeNum == "71" || playerCubeNum == "72" || playerCubeNum == "73" || playerCubeNum == "74" || playerCubeNum == "75"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "98" && (mousedCubeName == "78" || mousedCubeName == "79" || mousedCubeName == "80" || mousedCubeName == "81" || mousedCubeName == "82"))
+        else if (meYellow && cubeNum == "98" && (playerCubeNum == "78" || playerCubeNum == "79" || playerCubeNum == "80" || playerCubeNum == "81" || playerCubeNum == "82"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "07" && (mousedCubeName == "05" || mousedCubeName == "06")) //Snakes
+        else if (meYellow && cubeNum == "07" && (playerCubeNum == "05" || playerCubeNum == "06")) //Snakes
         {
             gameObject.GetComponent<Renderer>().material = yellowMat2;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "07" && (mousedCubeName == "01" || mousedCubeName == "02" || mousedCubeName == "04" || mousedCubeName == "08" || mousedCubeName == "09" || mousedCubeName == "10"))
+        else if (meYellow && cubeNum == "07" && (playerCubeNum == "01" || playerCubeNum == "02" || playerCubeNum == "04" || playerCubeNum == "08" || playerCubeNum == "09" || playerCubeNum == "10"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "13" && mousedCubeName == "12")
+        else if (meYellow && cubeNum == "13" && playerCubeNum == "12")
         {
             gameObject.GetComponent<Renderer>().material = yellowMat2;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "13" && (mousedCubeName == "07" || mousedCubeName == "08" || mousedCubeName == "09" || mousedCubeName == "10" || mousedCubeName == "13" || mousedCubeName == "14" || mousedCubeName == "16" || mousedCubeName == "17"))
+        else if (meYellow && cubeNum == "13" && (playerCubeNum == "07" || playerCubeNum == "08" || playerCubeNum == "09" || playerCubeNum == "10" || playerCubeNum == "13" || playerCubeNum == "14" || playerCubeNum == "16" || playerCubeNum == "17"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "12" && (mousedCubeName == "23" || mousedCubeName == "24" || mousedCubeName == "26" || mousedCubeName == "27"))
+        else if (meYellow && cubeNum == "12" && (playerCubeNum == "23" || playerCubeNum == "24" || playerCubeNum == "26" || playerCubeNum == "27"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "34" && (mousedCubeName == "30" || mousedCubeName == "31" || mousedCubeName == "32" || mousedCubeName == "33"))
+        else if (meYellow && cubeNum == "34" && (playerCubeNum == "30" || playerCubeNum == "31" || playerCubeNum == "32" || playerCubeNum == "33"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat2;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "34" && (mousedCubeName == "29" || mousedCubeName == "35"))
+        else if (meYellow && cubeNum == "34" && (playerCubeNum == "29" || playerCubeNum == "35"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "16" && (mousedCubeName == "71" || mousedCubeName == "72" || mousedCubeName == "73" || mousedCubeName == "74" || mousedCubeName == "75"))
+        else if (meYellow && cubeNum == "16" && (playerCubeNum == "71" || playerCubeNum == "72" || playerCubeNum == "73" || playerCubeNum == "74" || playerCubeNum == "75"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "26" && (mousedCubeName == "42" || mousedCubeName == "43" || mousedCubeName == "44" || mousedCubeName == "45" || mousedCubeName == "46"))
+        else if (meYellow && cubeNum == "26" && (playerCubeNum == "42" || playerCubeNum == "43" || playerCubeNum == "44" || playerCubeNum == "45" || playerCubeNum == "46"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "39" && (mousedCubeName == "78" || mousedCubeName == "79" || mousedCubeName == "80" || mousedCubeName == "81" || mousedCubeName == "82"))
+        else if (meYellow && cubeNum == "39" && (playerCubeNum == "78" || playerCubeNum == "79" || playerCubeNum == "80" || playerCubeNum == "81" || playerCubeNum == "82"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "75" && (mousedCubeName == "86" || mousedCubeName == "87" || mousedCubeName == "88" || mousedCubeName == "89" || mousedCubeName == "90" || mousedCubeName == "91"))
+        else if (meYellow && cubeNum == "75" && (playerCubeNum == "86" || playerCubeNum == "87" || playerCubeNum == "88" || playerCubeNum == "89" || playerCubeNum == "90" || playerCubeNum == "91"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (cubeNum == "70" && (mousedCubeName == "93" || mousedCubeName == "94" || mousedCubeName == "95" || mousedCubeName == "96" || mousedCubeName == "97" || mousedCubeName == "98"))
+        else if (meYellow && cubeNum == "70" && (playerCubeNum == "93" || playerCubeNum == "94" || playerCubeNum == "95" || playerCubeNum == "96" || playerCubeNum == "97" || playerCubeNum == "98"))
         {
             gameObject.GetComponent<Renderer>().material = yellowMat;
+            myOrderNum = orderRec;
+            iAmYellow = true;
         }
-        else if (meBlue && mousedCubeName == "100") //YOU WIN!!!
+        else if (meBlue && playerCubeNum == "100") //YOU WIN!!!
         {
             gameObject.GetComponent<Renderer>().material = greenMat;
+            iAmYellow = false;
         }
         else if (meBlue && cubeNum != "03" && cubeNum != "15" && cubeNum != "22" && cubeNum != "25" && cubeNum != "41" && cubeNum != "53" && cubeNum != "63" && cubeNum != "76" && cubeNum != "84"
             && cubeNum != "11" && cubeNum != "18" && cubeNum != "28" && cubeNum != "36" && cubeNum != "77" && cubeNum != "47" && cubeNum != "83" && cubeNum != "92" && cubeNum != "99")
         {
             gameObject.GetComponent<Renderer>().material = blueMat;
+            iAmYellow = false;
+        }
+        else if (player.GetComponent<PlayerManager>().playerCurrentTile == cubeNum) //Player is on the blue tile
+        {
+            gameObject.GetComponent<Renderer>().material = blueMat;
+            iAmYellow = false;
         }
         else
         {
             gameObject.GetComponent<Renderer>().material = defaultMaterial;
+            iAmYellow = false;
+            myOrderNum = 0;
         }
+    }
+
+    public void orderReceive(int i)
+    {
+        orderRec = i;
     }
 }
---
+++ Homework2/SnakesAndLadders/3D Game/PlayerManager.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour {
+
+    public string playerCurrentTile;
+    private GameObject[] cubes;
+    private GameObject[] yellowCubes;
+    private GameObject cube1;
+    private GameObject cube2;
+    private GameObject cube3;
+    private GameObject cube4;
+    private GameObject cube5;
+    private GameObject cube6;
+    [Tooltip("Main canvas rolled text")]
+    public Text canvasRolled;
+    [Tooltip("Main canvas rolls text")]
+    public Text canvasRolls;
+    public int rolls;
+
+    private void Awake()
+    {
+        gameObject.transform.position = new Vector3(1, 1, 1); //Start at cell 1
+        playerCurrentTile = "01";
+    }
+
+    void Start ()
+    {
+        yellowCubes = new GameObject[7];
+        rolls = 0;
+        if (cubes == null)
+        {
+            cubes = GameObject.FindGameObjectsWithTag("Cube");
+        }
+    }
+
+    void Update()
+    {    
+        foreach (GameObject currentCube in cubes)
+        {
+            int playerCubeNum = int.Parse(playerCurrentTile);
+            int currentCubeNum = int.Parse(currentCube.GetComponent<CubeManager>().cubeNum);
+
+            if (currentCubeNum == playerCubeNum + 1)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(1);
+            }
+            else if (currentCubeNum == playerCubeNum + 2)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(2);
+            }
+            else if (currentCubeNum == playerCubeNum + 3)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(3);
+            }
+            else if (currentCubeNum == playerCubeNum + 4)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(4);
+            }
+            else if (currentCubeNum == playerCubeNum + 5)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(5);
+            }
+            else if (currentCubeNum == playerCubeNum + 6)
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = true;
+                currentCube.GetComponent<CubeManager>().orderReceive(6);
+            }
+            else
+            {
+                currentCube.GetComponent<CubeManager>().meYellow = false;
+                currentCube.GetComponent<CubeManager>().orderReceive(0);
+            }
+        }
+    }
+
+    public void movePlayer()
+    {
+        foreach (GameObject currentCube in cubes)
+        {
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 1 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[1] = currentCube;
+            }
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 2 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[2] = currentCube;
+            }
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 3 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[3] = currentCube;
+            }
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 4 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[4] = currentCube;
+            }
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 5 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[5] = currentCube;
+            }
+            if (currentCube.GetComponent<CubeManager>().myOrderNum == 6 && currentCube.GetComponent<CubeManager>().iAmYellow)
+            {
+                yellowCubes[6] = currentCube;
+            }
+        }
+    
+        int moveSpaces = Random.Range(1, 6);
+        rolls++;
+        print(moveSpaces);
+        canvasRolled.text = "Rolled a " + moveSpaces.ToString();
+        canvasRolls.text = "Rolls: " + rolls.ToString();
+
+        if (moveSpaces == 1)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[1].transform.position.x, 1, yellowCubes[1].transform.position.z);
+            playerCurrentTile = yellowCubes[1].GetComponent<CubeManager>().cubeNum;
+        }
+        else if (moveSpaces == 2)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[2].transform.position.x, 1, yellowCubes[2].transform.position.z);
+            playerCurrentTile = yellowCubes[2].GetComponent<CubeManager>().cubeNum;
+        }
+        else if (moveSpaces == 3)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[3].transform.position.x, 1, yellowCubes[3].transform.position.z);
+            playerCurrentTile = yellowCubes[3].GetComponent<CubeManager>().cubeNum;
+        }
+        else if (moveSpaces == 4)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[4].transform.position.x, 1, yellowCubes[4].transform.position.z);
+            playerCurrentTile = yellowCubes[4].GetComponent<CubeManager>().cubeNum;
+        }
+        else if (moveSpaces == 5)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[5].transform.position.x, 1, yellowCubes[5].transform.position.z);
+            playerCurrentTile = yellowCubes[5].GetComponent<CubeManager>().cubeNum;
+        }
+        else if (moveSpaces == 6)
+        {
+            gameObject.transform.position = new Vector3(yellowCubes[6].transform.position.x, 1, yellowCubes[6].transform.position.z);
+            playerCurrentTile = yellowCubes[6].GetComponent<CubeManager>().cubeNum;
+        }
+    }
+}
